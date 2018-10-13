dcc559cd58c2f57283e47e4c7528a3cb271abd37
tossowski <timothy_ossowski@brown.edu>
2018-10-13 19:25:21 -0400

    First commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    private GameObject target;
+
+    private Rigidbody2D myrigidbody;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindWithTag("Player");
+        myrigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float playerY = target.transform.position.y;
+        transform.position = new Vector2(transform.position.x, playerY);
+        myrigidbody.velocity = new Vector2(speed, 0);
+	}
+}
---
+++ Assets/MoveController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+
+    public float speed;
+
+    public float jumpPower;
+
+    private Rigidbody2D myrigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+        myrigidbody = GetComponent<Rigidbody2D>();
+        myrigidbody.velocity = new Vector2(speed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myrigidbody.velocity = new Vector2(speed, myrigidbody.velocity.y);
+        if (Input.GetKeyDown("space")) {
+            myrigidbody.velocity += new Vector2(0, jumpPower);
+        }
+	}
+}
