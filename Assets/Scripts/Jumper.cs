0ab45cc8582bcaadfcd65baefcb2d3ee8944456b
tossowski <timothy_ossowski@brown.edu>
2018-10-13 20:23:41 -0400

    Changed file structure

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MoveController {
+    public float jumpPower;
+    private bool jumping;
+
+    void Update()
+    {
+        myrigidbody.velocity = new Vector2(speed, myrigidbody.velocity.y);
+        if (Input.GetKeyDown("space") && !jumping)
+        {
+            Action();
+        }
+    }
+
+    public override void Action()
+    {
+        myrigidbody.velocity += new Vector2(0, jumpPower);
+        jumping = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        jumping = false;
+    }
+}
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+    public float speed;
+    public Rigidbody2D myrigidbody;
+
+	// Use this for initialization
+	void Start () {
+        myrigidbody = GetComponent<Rigidbody2D>();
+        myrigidbody.velocity = new Vector2(speed, 0);
+	}
+	
+	// Update is called once per frame
+	
+
+    public virtual void Action()
+    {
+        
+    }
+
+    
+}
