e407a72c4402ea3b75e4c012507d0d20f29a18f8
tossowski <timothy_ossowski@brown.edu>
2018-11-03 17:46:42 -0400

    Added 3rd Character

---
+++ Assets/Scripts/Dasher.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dasher : MoveController {
+
+    private Animator anim;
+    private bool dashing;
+    public float dashSpeed;
+    private float maxspeed;
+	// Use this for initialization
+	void Start () {
+        base.Start();
+        anim = GetComponent<Animator>();
+        maxspeed = dashSpeed + speed;
+	}
+
+    void Update()
+    {
+        myrigidbody.velocity = new Vector2(speed, myrigidbody.velocity.y);
+        if (Input.GetKeyDown("space"))
+        {
+            Action();
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            base.changeCharacter(ToSwitch);
+            Destroy(gameObject);
+        }
+
+    }
+
+    public override void Action()
+    {
+        dashing = true;
+        anim.SetBool("Dashing", true);
+        speed = maxspeed;
+    }
+
+    public void setDashing()
+    {
+        dashing = false;
+        anim.SetBool("Dashing", false);
+        speed -= dashSpeed;
+    }
+}
